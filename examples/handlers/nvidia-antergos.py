dcbf32a09b36175b8d9eda265c87f06abc5163c0
Alexandre Filgueira <faidoc@gmail.com>
2013-05-30 22:27:19 +0200

    Added some preliminary nvidia handler working with Antergos Also added ubuntu-drivers-common source to try to port nvidiadetector to Antergos

---
+++ examples/handlers/nvidia-antergos.py
@@ -0,0 +1,73 @@
+# (c) 2008 Canonical Ltd.
+# Author: Martin Pitt <martin.pitt@ubuntu.com>
+# License: GPL v2 or later
+
+import logging
+
+from jockey.handlers import KernelModuleHandler
+from jockey.xorg_driver import XorgDriverHandler
+from jockey.oslib import OSLib
+import XKit
+#from NvidiaDetector.nvidiadetector import NvidiaDetection
+
+# dummy stub for xgettext
+def _(x): return x
+
+class NvidiaDriver(XorgDriverHandler):
+    def __init__(self, backend):
+        self._free = False
+        # use "None" as driver_package, since we have several;
+        # LocalKernelModulesDriverDB overwrites it later with the correct
+        # package from the modalias lists
+        XorgDriverHandler.__init__(self, backend, 'nvidia', None,
+            'nvidia', 'nv', {'NoLogo': 'True'},
+            add_modules=[], disable_modules=[],
+            remove_modules=[],
+            name=_('NVIDIA accelerated graphics driver'),
+            description=_('3D-accelerated proprietary graphics driver for '
+                'NVIDIA cards.'),
+            rationale=_('This driver is required to fully utilise '
+                'the 3D potential of NVIDIA graphics cards, as well as provide '
+                '2D acceleration of newer cards.\n\n'
+                'If you wish to enable desktop effects, this driver is '
+                'required.\n\n'
+                'If this driver is not enabled, you will not be able to '
+                'enable desktop effects and will not be able to run software '
+                'that requires 3D acceleration, such as some games.'))
+
+        self._recommended = None
+        self._do_rebind = False
+        
+    def id(self):
+        '''Return an unique identifier of the handler.'''
+
+        if self.package:
+            self.version = self.package.split('-')[-1]
+            i = 'xorg:' + self.module + '-' + self.version
+        else:
+            i = 'xorg:' + self.module
+        if self.driver_vendor:
+            i += ':' + self.driver_vendor.replace(' ', '_')
+        return i
+    
+    def recommended(self):
+        if self._recommended == None:
+            #nd = NvidiaDetection()
+            #self._recommended = self.package == nd.selectDriver()
+            self._recommended = self.package == 'nvidia'
+        return self._recommended
+
+    def enabled(self):
+        return KernelModuleHandler.enabled(self)
+
+    def enables_composite(self):
+        '''Return whether this driver supports the composite extension.'''
+
+        # When using an upstream installation, or -new/-legacy etc., we already
+        # have composite
+        if KernelModuleHandler.module_loaded('nvidia'):
+            logging.debug('enables_composite(): already using nvidia driver from nondefault package')
+            return False
+
+        # neither vesa nor nv support composite, so safe to say yes here
+        return True
